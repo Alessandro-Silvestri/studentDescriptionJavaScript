a91be5c13a4c627d9be7f498191a5c169aae428b
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2025-01-02 16:55:42 +0100

    upload html boiler plate and class pseudo code

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+Student description and score and calculator
+
+CLASS:
+    properties:
+        name
+        surname
+        address
+        postcode
+        country
+        remote lesson (boolean)
+        score [5, 6, 7, 8, 9]
+    methods:
+        add score
+        remove score
+        clear score
+        retrieve information
+        calculate average
+*/
