dd7efa0b742e82ba0159e8e7e29b849f62eb7ff9
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2025-01-02 17:47:49 +0100

    class, properties and methods created, everything work

--- script.js
+++ script.js
@@ -17,3 +17,58 @@ CLASS:
         retrieve information
         calculate average
 */
+
+class Student {
+    constructor() {
+        this.firstName = "";
+        this.lastName = "";
+        this.address = "";
+        this.postcode = "";
+        this.country = "";
+        this.remoteLessons = false;
+        this.score = [];
+    }
+
+    addScore(grade) {
+        this.score.push(grade);
+    }
+
+    clearScore() {
+        this.score = []
+    }
+
+    calculateAverage() {
+        let sum = 0;
+        this.score.forEach(grade => { sum += grade })
+        let average = sum / this.score.length;
+        if (isNaN(average)){
+            return 0;
+        }
+        return average.toFixed(2);
+    }
+
+    getStudentInfo(){
+        console.log(`First name: ${this.firstName}`);
+        console.log(`Last name: ${this.lastName}`);
+        console.log(`Address: ${this.address}`);
+        console.log(`Postcode: ${this.postcode}`);
+        console.log(`Country: ${this.country}`);
+        console.log(`RemoteLessons: ${this.remoteLessons}`);
+        console.log(`Score: ${this.score}`);
+        console.log(`Average score: ${this.calculateAverage()}`);
+    }
+}
+
+let alex = new Student()
+
+alex.firstName = "Alessandro";
+alex.lastName = "Silvestri";
+alex.address = "Huddersfield - 1 Western Road";
+alex.postcode = "HD45TH";
+alex.country = "United Kingdom";
+alex.remoteLessons = true;
+alex.addScore(4);
+alex.addScore(5);
+alex.addScore(8);
+// alex.clearScore();
+console.log(alex.calculateAverage());
